c46fa8500c0d502a24678747127c1b9dede98f3e
David Hirschfeld <david.hirschfeld@stanwell.com>
2020-09-18 09:33:25 +1000

    wip

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,8 @@ export GOBIN=$GOBIN
 
 go build -v -o ${target_gobin}${PKG_NAME}${target_goexe} -ldflags "$LDFLAGS"
 
+test -f $PREFIX/bin/go-licenses
+
 go get -v github.com/google/go-licenses
 $GOBIN/go-licenses save $SRC_DIR --save_path="$RECIPE_DIR/thirdparty_licenses/"
 
